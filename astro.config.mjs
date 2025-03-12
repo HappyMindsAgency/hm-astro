9bad3a079425e94dbc40cd9243e1662e6b8a6a5d
dariotronic <darioerste@gmail.com>
2025-03-12 17:01:04 +0100

    Stilizzato aspetto desktop di Navbar e VideoHeader + integrazione font locali + SCSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,13 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    vite: {
+      css: {
+        preprocessorOptions: {
+          scss: {
+            // Se hai delle variabili globali puoi aggiungere:
+          }
+        }
+      }
+    }
 });
